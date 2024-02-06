273e567fd05b3c6c9b19e0e95564ada34c7d3507
EXB04891 <agustina.aliciardi@darwoft.com>
2024-02-06 17:51:24 -0300

    Add functions

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const sumar = (a, b) => {
   return a + b;
 };
 
+const resta = (a, b) => {
+  return a - b;
+};
+
+const multi = (a, b) => {
+  return a * b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
 module.exports = { sumar };
